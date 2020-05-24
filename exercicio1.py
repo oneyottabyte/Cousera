17e18a046174c607d0e790373470385171403ad7
Dorian Vieira <junhinhodorian@gmail.com>
2020-05-24 13:47:09 -0300

    Formulas adicionadas e gitignore criado

--- exercicio1.py
+++ exercicio1.py
@@ -1,4 +1,6 @@
-def main ():
-    ladoquadrado = int(input("Qual o lado do quadrado?"))
-
+def main():
+    ladoquadrado = float(input("Digite o tamanho do lado do quadrado: "))
+    area = ladoquadrado**2
+    perimetro = ladoquadrado*4
+    print("perímetro:",perimetro,"- área:",area)
 main()
